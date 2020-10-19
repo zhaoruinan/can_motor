0dd385db1ea66812a41d85681e18c47c10a6b2c5
zhaoruinan <z_ruinan@hotmail.com>
2020-10-19 14:21:31 +0900

    add TCP test

---
+++ motor_can.py
@@ -0,0 +1,32 @@
+import wx
+import can
+import time
+bus = can.interface.Bus(bustype='kvaser', channel='0', bitrate=1000000)
+def int_to_char(n):
+    b = [0x00, 0x00, 0x00, 0x00]
+    b[0] =b[0]+ ((n & 0xff000000) >> 24)
+    b[1] =b[1]+ ((n & 0xff0000) >> 16)
+    b[2] =b[2]+ ((n & 0xff00) >> 8)
+    b[3] =b[3]+ (n & 0xff)
+    return b
+def motor_set_speed(node,speed):
+    n = speed*100
+    b = int_to_char(n)
+    print(b)
+    msg = can.Message(arbitration_id=0x140+node, data=[0xA2, 0x00, 0x00, 0x00, 0x00+b[3], 0x00+b[2], 0x00+b[1], 0x00+b[0]], is_extended_id=False)
+    bus.send(msg)
+def motor_set_speed_m(node,speed):
+    n = -speed*100
+    b = int_to_char(n)
+    print(b)
+    msg = can.Message(arbitration_id=0x140+node, data=[0xA2, 0x00, 0x00, 0x00, 0x00+b[3], 0x00+b[2], 0x00+b[1], 0x00+b[0]], is_extended_id=False)
+    bus.send(msg)
+def motor_read_pos(node):
+    msg = can.Message(arbitration_id=0x140+node, data=[0x90, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00], is_extended_id=False)
+    bus.send(msg)
+    recv = bus.recv()
+    print(recv)
+    try:
+        return(recv.data[3]*256+recv.data[2])
+    except:
+        print("please make sure CAN cable is working")
---
+++ test/server.py
@@ -0,0 +1,20 @@
+import socket
+
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+#from motor_can import motor_set_speed,motor_set_speed_m,motor_read_pos
+def data_process(data):
+    return True
+while True:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print('Connected by', addr)
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                print(data)
+                conn.sendall(b'gay')
